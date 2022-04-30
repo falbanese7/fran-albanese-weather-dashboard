429703a5ec7b1b72c4bbb6e827c49befc15ea6cb
Francesco Albanese <falbanese96@gmail.com>
2022-04-30 16:07:19 -0400

    Final draft that completes the remaining functionality of the application

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# fran-albanese-weather-dashboard
+# Fran-Albanese's-Weather-Dashboard
+
+Welcome to my weather dashboard! In this README file are instructions on how to use the application.
+
+Link to application - https://falbanese96.github.io/fran-albanese-weather-dashboard/
+
+## Searching for a city
+
+To start, the user will need to type in their desired city name. The name must be spelled correctly or the application will yield no result. Once the user inputs text, they will need to click the "Search" button.
+
+![Screen Shot 2022-04-30 at 3 15 15 PM](https://user-images.githubusercontent.com/98659683/166119955-34c3e608-602e-486f-8052-23b740bd86bd.png)
+
+## Seeing Results
+
+Once the "Search" button is clicked, the user will see a larger section displaying the current day's weather. The values include:
+
+- The name of their selected city and the current day's date
+- A small image representing the weather conditions
+- Temperature in Farenheit
+- Wind speed in miles per hour
+- Humidity percentage
+- The UV Index
+
+The user should also see a section under the current day's weather that displays the five day forecast for their chosen city. The metrics shown here include:
+
+- The forecasted day's date
+- A small image representing the weather conditions
+- Temperature in Farenheit
+- Wind speed in miles per hour
+- Humidity percentage
+
+![Screen Shot 2022-04-30 at 3 15 44 PM](https://user-images.githubusercontent.com/98659683/166119988-837221c6-04f7-4459-a32d-1d718bbc6b47.png)
+
+The UV Index value will change color indicating whether the conditions are favorable (green), moderate (yellow), or severe (red).
+
+## Search History
+
+The user's search history will be saved to their Local Storage and displayed under the search button in a vertical format. The search results are clickable buttons that the user can click to be presented with that corresponding city's weather once again.
+
+![gif showing how to use recent history buttons](https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/0615eb28-9257-49ce-ab4a-1d8ee0ec186c.gif?ClientID=vimeo-core-prod&Date=1651346496&Signature=da6021a0a18092199e9750526ead4aee887fb3ef)
+
+## Clearing the Search History
+
+When the user wants to reset their search history, they can click the "Clear Recent Searches" button. The application will clear the items it saved in their Local Storage, remove the previously searched city buttons, and clear the weather results.
+
+![gif showing how to use the clear history button](https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/b8582720-fa02-4437-b4ac-a09df6f3475f.gif?ClientID=vimeo-core-prod&Date=1651347860&Signature=9c5ba969cb56d489c4a6bf43e715aeb608109aeb)
+
+## Credits and Relevant Frameworks
+
+Developer - Francesco Albanese
+
+Styling:
+Bootstrap
+Google Fonts
+
+Interactivity:
+jQuery
+Moment.js
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,18 @@
+// Declaring variables needed from html elements.
 const userInput = document.getElementById('search-input');
 const searchForm = document.getElementById('search-form');
 const resultEl = document.getElementById('results');
 const displayInput = document.getElementById('search-text');
 const displayResults = document.getElementById('search-content');
-const displayHistory = document.getElementById('search-section');
+const displayHistory = document.getElementById('history');
+const buttonList = document.createElement('button');
 const fiveDay = document.getElementById('five-day');
+const clearRecentSearchBtn = document.getElementById('clear-history')
+
+// The current day's date.
 let day = moment().format('M/DD/YYYY');
 
+// This function calls the OpenWeather Geocoding API in order to gather the latitudinal and longitudinal coordinates of the user's selected city.
 function getCoordinates() {
     const cityConvert = 'http://api.openweathermap.org/geo/1.0/direct?q='+userInput.value+'&limit=1&appid=1158e7e2e205b37a0d1f795667970aaf'
  
@@ -15,10 +21,11 @@ function getCoordinates() {
         return response.json();
     })
     .then(function (data) {
-     renderWeatherData(data);
+     renderWeatherData(data); // Calling the renderWeatherData function so we can use the results from the Geocoding call in the renderWeatherData function.
     })
  }
 
+//This function uses the latitudinal and longitudinal data from the previous function inside of the OneCall API from OpenWeather. 
 function renderWeatherData(data) {
     const coordinates = data;
     console.log(coordinates);
@@ -35,6 +42,7 @@ function renderWeatherData(data) {
     }).then(function (data) {
         console.log(data);
         refreshResults();
+        // Creating HTML elements
         const weatherData = document.createElement('div');
         const temp = document.createElement('p');
         const wind = document.createElement('p');
@@ -43,6 +51,7 @@ function renderWeatherData(data) {
         const uvSpan = document.createElement('span');
         const weatherIcon = data.current.weather[0].icon;
 
+        // Giving text and HTML values to the previously declared variables.
         displayInput.innerHTML = '<h1>'+ cityName + ' ' + '(' + day + ')' +'<img src=http://openweathermap.org/img/wn/'+ weatherIcon + '@2x.png>' + '</h1>';
         temp.textContent = 'Temp: ' + data.current.temp + '°F';
         wind.textContent = 'Wind speed: ' + data.current.wind_speed + ' mph';
@@ -50,6 +59,7 @@ function renderWeatherData(data) {
         uvIndex.textContent = 'UV Index: ';
         uvSpan.textContent = data.current.uvi;
 
+        // Using if else to automatically change the uvSpan's class which contains the appropriate color for the value.
         if (data.current.uvi < 3) {
             uvSpan.classList.add('lowUVI')
         } else if ( data.current.uvi < 5) {
@@ -60,6 +70,7 @@ function renderWeatherData(data) {
             console.log('UV Index is astronomically high')
         }
 
+        // Appending variables to parents to render them to the page.
         uvIndex.appendChild(uvSpan)
         weatherData.appendChild(temp);
         weatherData.appendChild(wind);
@@ -69,6 +80,7 @@ function renderWeatherData(data) {
         displayResults.appendChild(weatherData);
         displayResults.classList.add('border', 'border-dark');
 
+    // Calling OneCall API again for the five day forecast.    
     let fiveDayAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=current,minutely,hourly,alerts&appid=1158e7e2e205b37a0d1f795667970aaf&units=imperial'
 
     fetch(fiveDayAPI)
@@ -80,6 +92,7 @@ function renderWeatherData(data) {
         const fiveDayTitle = document.getElementById('five-day-title')
         fiveDayTitle.innerHTML = 'Five Day Forecast'
 
+        // This for loop creates 5 cards that render each of the day's forecast in the five day forecast.
         for (i = 1; i < 6; i++) {
             const fiveDayCol = document.createElement('div');
             fiveDayCol.setAttribute('class', 'col-12 col-md-6 col-lg mb-3')
@@ -116,20 +129,78 @@ function renderWeatherData(data) {
 })
 }
 
+// This function will clear the currently displayed results when the user inputs a new result.
 function refreshResults() {
     displayInput.innerHTML = '';
     displayResults.innerHTML = '';
     fiveDay.innerHTML = '';
 }
 
+// Checking for a value in the userInput and firing several functions when the "Search" button is clicked.
 function handleSearchSubmit(event) {
     event.preventDefault();
 
     if (!userInput) {
         alert(response.message)
     }
-
     getCoordinates();
+    addInputToLocal(userInput);
+    renderHistory();  
 }
 
+// Clears the local storage and recently searched buttons.
+function clearRecentSearch () {
+    localStorage.clear();
+    displayHistory.innerHTML = '';
+    location.reload();
+}
+
+// Renders recent search buttons to the page.
+function renderHistory() {
+    const readData = localStorage.getItem('userInputs');
+    const readDataArr = JSON.parse(readData) || [];
+    displayHistory.innerHTML = '';
+
+    for (let i = 0; i < readDataArr.length; i++) {
+        let historyButton = document.createElement('button');
+        historyButton.setAttribute('class', 'custom-clear btn btn-secondary')
+        historyButton.textContent = readDataArr[i];
+        
+        displayHistory.appendChild(historyButton);
+    }
+    
+}
+
+// Checking for clicks on the recently searched buttons.
+function historyClickHandler(event) {
+    const element = event.target;
+
+    if (element.matches('button')) {
+        userInput.value = element.textContent;
+        buttonList.click();
+    }   
+}
+
+// Saves user inputs to local storage and reads the data.
+function addInputToLocal(userInput){
+    let userInputs = []; 
+    const readData = localStorage.getItem('userInputs');
+    if (readData) {
+        userInputs = JSON.parse(readData);
+    }
+    if (!userInputs.includes(userInput.value)){
+        userInputs.push(userInput.value);
+        localStorage.setItem('userInputs', JSON.stringify(userInputs));
+    } 
+}
+
+// Event listeners for various buttons and form.
 searchForm.addEventListener('submit', handleSearchSubmit);
+
+buttonList.addEventListener('click', handleSearchSubmit);
+
+clearRecentSearchBtn.addEventListener('click', clearRecentSearch);
+
+displayHistory.addEventListener('click', historyClickHandler);
+
+renderHistory();
